15a30659c27c1b65233806a16558056901b8587e
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-03-20 09:26:57 -0500

    Se agrega el boton submit a el formulario para capturar los datos

--- src/app/seguridad/registrar/registrar.component.ts
+++ src/app/seguridad/registrar/registrar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
-  styleUrl: './registrar.component.css'
+  styleUrl: './registrar.component.css',
 })
 export class RegistrarComponent {
-
+  registrarUsuario(form: NgForm) {
+    console.log(form);
+  }
 }
